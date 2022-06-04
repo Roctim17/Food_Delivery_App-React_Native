41d94d517e44f97c3efd906c98ce16bae2323f10
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-06-04 13:08:25 +0600

    start

---
+++ Screen/OrderDelivery.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function OrderDelivery() {
+    return (
+        <View>
+            <Text>OrderDelivery</Text>
+        </View>
+    )
+}
